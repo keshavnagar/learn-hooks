62bf6fe1034e280d3b81a4202cf8b932ab2349a5
keshavnagar <keshavnagar887@gmail.com>
2025-08-22 12:28:01 +0530

    i organize the things like oneUseState component that conain only learning of one useState thing and we update multiple times

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import { useState } from "react";
-
+import oneUseState from "./components/oneUseState";
 const App = () => {
-  const [color, setColor] = useState("blue");
+  
   return (
     <>
       <h1>Learn Hooks</h1>
-      <p>My favorite color is {color}</p>
-      <button onClick={() => setColor("yellow")}>
-        change the color to yellow
-      </button>
-      <button onClick={() => setColor("green")}>
-        change the color to green
-      </button>
-      <button onClick={() => setColor("red")}>change the color to red</button>
-      <button onClick={() => setColor("black")}>
-        change the color to black
-      </button>
-      <button onClick={() => setColor("white")}>
-        change the color to white
-      </button>
-      <button onClick={() => setColor("blue")}>
-        change the color to blue
-      </button>
+      <oneUseState/>
     </>
   );
 };
---
+++ src/components/oneUseState.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+const oneUseState = () => {
+  const [color, setColor] = useState("blue");
+  return (
+    <>
+      <p>My favorite color is {color}</p>
+      <button onClick={() => setColor("yellow")}>
+        change the color to yellow
+      </button>
+      <button onClick={() => setColor("green")}>
+        change the color to green
+      </button>
+      <button onClick={() => setColor("red")}>change the color to red</button>
+      <button onClick={() => setColor("black")}>
+        change the color to blacks
+      </button>
+      <button onClick={() => setColor("white")}>
+        change the color to white
+      </button>
+      <button onClick={() => setColor("blue")}>change the color to blue</button>
+    </>
+  );
+};
+
+export default oneUseState;
